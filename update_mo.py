9cbb5b51176340e08ef6e66c4731b9398d6e90e4
KulDmiVi <kulDmiVi@gmail.com>
2025-12-23 12:52:13 +0300

    featch: Утилита загрузки и обновления справочника Организаций

--- update_mo.py
+++ update_mo.py
@@ -26,6 +26,10 @@ def util_mo_updater():
     system_id = system_data[0]
     moDeptName = 'Органы исполнительной власти субъектов Российской Федерации, осуществляющие функции в области здравоохранения'
     with open('load_org.log', 'a', encoding='utf8') as log_file:
+        if not system_id:
+            msg = f"Не найден идентификатор urn:oid:1.2.643.5.1.13.2.1.1.178\n"
+            print(msg)
+            log_file.write(msg)
         for row in csv_data:
             if row_filter(row):
                 org_data = search_mo(row)
@@ -33,21 +37,21 @@ def util_mo_updater():
                     update_data = cheсk_update(org_data, row)
 
                     if update_data:
-                        update_organisation(org_data['id'], update_data)
+                        #update_organisation(org_data['id'], update_data)
                         msg = f"Update organisation: {row['oid']}. data {update_data}\n"
                         print(msg)
                         log_file.write(msg)
-
                     identification_data = get_organization_indentification(org_data['id'], system_id[0])
+
                     if not identification_data:
                         insert_organization_indentification(org_data['id'], system_id[0], row)
                         msg = f"Insert identification: {row['oid']}\n"
                         print(msg)
                         log_file.write(msg)
                 else:
-                    if row['deleteDate'] != '' and row['moDeptName'] != moDeptName:
-                        insert_organisation(row)
-                        # insert_organization_indentification(org_id, system_id[0], row)
+                    if row['deleteDate'] != '' and row['moDeptName'] == moDeptName:
+                        org_id = insert_organisation(row)
+                        #insert_organization_indentification(org_id, system_id[0], row)
                         msg = f"Insert organisation: {row['nameFull']} {row['moAgencyKind']} \n"
                         print(msg)
                         log_file.write(msg)
@@ -103,4 +107,7 @@ def check_organisation_kind(data, organisation_kind):
 def search_mo(row):
     """Поиск организации в БД по значению из справочника"""
     org_data = get_organisation_info_by_rekviz(row['inn'], row['kpp'], row['ogrn'])
-    return org_data[0]
+    if org_data:
+        return org_data[0]
+    else:
+        return None
